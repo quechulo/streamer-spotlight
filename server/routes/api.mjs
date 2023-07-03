cd789ec35aa2fb8278dab80f7a4cb8a812f84817
quechulo <aawangardowy@gmail.com>
2023-07-03 14:43:56 +0200

    fixed minor errors

--- server/routes/api.mjs
+++ server/routes/api.mjs
@@ -5,51 +5,50 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 router.get("/streamers", async (req, res) => {
-  let collection = await db.collection("streamers");
-  let results = await collection.find({}).limit(50).toArray();
-
-  res.send(results).status(200);
+  try {
+    let collection = await db.collection("streamers");
+    let results = await collection.find({}).limit(50).toArray();
+    
+    res.send(results).status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 router.get("/streamer/:id", async (req, res) => {
-  let collection = await db.collection("streamers");
-  let query = { _id: new ObjectId(req.params.id) };
-  let result = await collection.findOne(query);
-
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+  try {
+    let collection = await db.collection("streamers");
+    let query = { _id: new ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+    
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 router.post("/streamers", async (req, res) => {
-  let collection = await db.collection("streamers");
-  let newDocument = req.body;
-  console.log(req.body)
-  newDocument.date = new Date();
-  let result = await collection.insertOne(newDocument);
-  res.send(result).status(204);
+  try {
+    let collection = await db.collection("streamers"); 
+    let newDocument = req.body;
+    newDocument.date = new Date();
+    let result = await collection.insertOne(newDocument);
+    res.send(result).status(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 router.put("/streamers/:id/vote", async (req, res) => {
-  let collection = await db.collection("streamers");
-  let query = { _id: new ObjectId(req.params.id) };
-  let update = { $inc: { upvotes: 1 } };
-
-  collection.updateOne(query, update, (err, result) => {
-    if (err) {
-      console.error("Error updating document", err);
-      res.send(err).status(500);
-    }
-    console.log("Document updated successfully");
-    console.log(result);
-    res.send(result).status(204);
-  });
-});
-
-router.put("/streamers/:id/downvote", async (req, res) => {
+  try {
     let collection = await db.collection("streamers");
     let query = { _id: new ObjectId(req.params.id) };
-    let update = { $inc: { downvotes: 1 } };
-  
+    let update = { $inc: { upvotes: 1 } };
+    
     collection.updateOne(query, update, (err, result) => {
       if (err) {
         console.error("Error updating document", err);
@@ -59,6 +58,32 @@ router.put("/streamers/:id/downvote", async (req, res) => {
       console.log(result);
       res.send(result).status(204);
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.put("/streamers/:id/downvote", async (req, res) => {
+  try {
+
+    let collection = await db.collection("streamers");
+    let query = { _id: new ObjectId(req.params.id) };
+    let update = { $inc: { downvotes: 1 } };
+    
+    collection.updateOne(query, update, (err, result) => {
+      if (err) {
+        console.error("Error updating document", err);
+        res.send(err).status(500);
+      }
+      console.log("Document updated successfully");
+      console.log(result);
+      res.send(result).status(204);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
   });
 
 export default router;
